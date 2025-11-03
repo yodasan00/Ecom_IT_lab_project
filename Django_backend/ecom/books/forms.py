0c9c0360aa37ed3781ee681baa09c4c4ee73977c
Yaad <yaadgurung90@gmail.com>
2025-11-04 00:24:50 +0530

    cart feature added

--- Django_backend/ecom/books/forms.py
+++ Django_backend/ecom/books/forms.py
@@ -5,8 +5,8 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['author_name', 'book_name', 'price']
-        widgets = {
-            'author_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Author Name'}),
-            'book_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Book Name'}),
-            'price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Price'}),
-        }
+        # widgets = {
+        #     'author_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Author Name'}),
+        #     'book_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Book Name'}),
+        #     'price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Price'}),
+        # }
--- Django_backend/ecom/books/urls.py
+++ Django_backend/ecom/books/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('add/', views.add_book, name='add_book'),
     path('edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('display/', views.book_display, name='book_display'),
+    path('add-to-cart/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('buy/<int:book_id>/', views.buy_now, name='buy_now'),
+    path('remove/<int:book_id>/', views.remove, name='remove'),
+    path('buy-now/<int:book_id>/', views.buy_now, name='buy_now'),
+    path('payment/', views.payment_page, name='payment_page'),
+    path('order-confirmed/', views.order_confirmed, name='order_confirmed'),
 ]
